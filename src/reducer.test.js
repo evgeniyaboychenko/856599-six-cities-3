1905604ee1535b5e6bc6247a3d3c86b1cba683f3
evgeniyaboychenko <evgeniyaboychenko@gmail.com>
2020-03-10 14:55:41 +0700

    правка тестов

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,14 +1,14 @@
 import {reducer, ActionCreator, ActionType} from "./reducer.js";
-// import configureStore from "redux-mock-store";
-// const mockStore = configureStore([]);
-// const middlewares = []
+import configureStore from "redux-mock-store";
+import {cities, offers} from './mocks/offers.js';
 
-const OFFER_IMAGES = [`room.jpg`, `apartment-01.jpg`];
-const OFFER_NAMES = [`Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
-const OFFER_TYPES = [`apartment`, `privet room`];
+const mockStore = configureStore([]);
+// const OFFER_IMAGES = [`room.jpg`, `apartment-01.jpg`];
+// const OFFER_NAMES = [`Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`];
+// const OFFER_TYPES = [`apartment`, `privet room`];
 
-const CITIES = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
-const COORDINATES_CITY = [[48.856663, 2.351556], [50.930779, 6.938399], [50.854283, 4.352131], [52.373057, 4.892557], [53.552645, 9.966287], [51.230569, 6.787428]];
+// const CITIES = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
+// const COORDINATES_CITY = [[48.856663, 2.351556], [50.930779, 6.938399], [50.854283, 4.352131], [52.373057, 4.892557], [53.552645, 9.966287], [51.230569, 6.787428]];
 
 const SortType = {
   DEFAULT: `Popular`,
@@ -16,108 +16,108 @@ const SortType = {
   HIGH_TO_LOW: `Price: high to low`,
   TOP_RATED_FIRST: `Top rated first`
 };
-const cities = [{
-  id: `1`,
-  name: CITIES[0],
-  coordinatesCity: COORDINATES_CITY[0],
-},
-{
-  id: `2`,
-  name: CITIES[1],
-  coordinatesCity: COORDINATES_CITY[1],
-},
-{
-  id: `3`,
-  name: CITIES[2],
-  coordinatesCity: COORDINATES_CITY[2],
-}
-];
+// const cities = [{
+//   id: `1`,
+//   name: CITIES[0],
+//   coordinatesCity: COORDINATES_CITY[0],
+// },
+// {
+//   id: `2`,
+//   name: CITIES[1],
+//   coordinatesCity: COORDINATES_CITY[1],
+// },
+// {
+//   id: `3`,
+//   name: CITIES[2],
+//   coordinatesCity: COORDINATES_CITY[2],
+// }
+// ];
 
-const offers = [
-  {
-    id: `1`,
-    cityId: `1`,
-    photos: OFFER_IMAGES,
-    name: OFFER_NAMES[0],
-    rating: 2,
-    price: 100,
-    type: OFFER_TYPES[0],
-    isPremium: true,
+// const offers = [
+//   {
+//     id: `1`,
+//     cityId: `1`,
+//     photos: OFFER_IMAGES,
+//     name: OFFER_NAMES[0],
+//     rating: 2,
+//     price: 100,
+//     type: OFFER_TYPES[0],
+//     isPremium: true,
 
-    descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-    countRooms: 2,
-    maxGuests: 2,
-    appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-    coordinates: [48.856663, 2.351556],
-    owner: {
-      avatar: `avatar-angelina.jpg`,
-      name: `Angelina`,
-      isSuper: false,
-    },
-  },
-  {
-    id: `4`,
-    cityId: `1`,
-    photos: OFFER_IMAGES,
-    name: OFFER_NAMES[0],
-    rating: 2,
-    price: 200,
-    type: OFFER_TYPES[0],
-    isPremium: true,
+//     descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
+//     countRooms: 2,
+//     maxGuests: 2,
+//     appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
+//     coordinates: [48.856663, 2.351556],
+//     owner: {
+//       avatar: `avatar-angelina.jpg`,
+//       name: `Angelina`,
+//       isSuper: false,
+//     },
+//   },
+//   {
+//     id: `4`,
+//     cityId: `1`,
+//     photos: OFFER_IMAGES,
+//     name: OFFER_NAMES[0],
+//     rating: 2,
+//     price: 200,
+//     type: OFFER_TYPES[0],
+//     isPremium: true,
 
-    descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-    countRooms: 2,
-    maxGuests: 2,
-    appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-    coordinates: [48.856663, 2.351556],
-    owner: {
-      avatar: `avatar-angelina.jpg`,
-      name: `Angelina`,
-      isSuper: false,
-    }
-  },
-  {
-    id: `2`,
-    cityId: `2`,
-    photos: OFFER_IMAGES,
-    name: OFFER_NAMES[1],
-    rating: 3,
-    price: 120,
-    type: OFFER_TYPES[1],
-    isPremium: false,
+//     descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
+//     countRooms: 2,
+//     maxGuests: 2,
+//     appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
+//     coordinates: [48.856663, 2.351556],
+//     owner: {
+//       avatar: `avatar-angelina.jpg`,
+//       name: `Angelina`,
+//       isSuper: false,
+//     }
+//   },
+//   {
+//     id: `2`,
+//     cityId: `2`,
+//     photos: OFFER_IMAGES,
+//     name: OFFER_NAMES[1],
+//     rating: 3,
+//     price: 120,
+//     type: OFFER_TYPES[1],
+//     isPremium: false,
 
-    descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-    countRooms: 2,
-    maxGuests: 2,
-    appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-    coordinates: [48.856663, 2.351556],
-    owner: {
-      avatar: `avatar-angelina.jpg`,
-      name: `Angelina`,
-      isSuper: true,
-    },
-  },
-  {
-    id: `3`,
-    cityId: `2`,
-    photos: OFFER_IMAGES,
-    name: OFFER_NAMES[1],
-    rating: 2,
-    price: 220,
-    type: OFFER_TYPES[1],
-    isPremium: false,
+//     descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
+//     countRooms: 2,
+//     maxGuests: 2,
+//     appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
+//     coordinates: [48.856663, 2.351556],
+//     owner: {
+//       avatar: `avatar-angelina.jpg`,
+//       name: `Angelina`,
+//       isSuper: true,
+//     },
+//   },
+//   {
+//     id: `3`,
+//     cityId: `2`,
+//     photos: OFFER_IMAGES,
+//     name: OFFER_NAMES[1],
+//     rating: 2,
+//     price: 220,
+//     type: OFFER_TYPES[1],
+//     isPremium: false,
 
-    descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-    countRooms: 6,
-    maxGuests: 6,
-    appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-    coordinates: [48.856663, 2.351556],
-    owner: {
-      avatar: `avatar-angelina.jpg`,
-      name: `Angelina`,
-      isSuper: true,
-    },
-  }];
+//     descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
+//     countRooms: 6,
+//     maxGuests: 6,
+//     appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
+//     coordinates: [48.856663, 2.351556],
+//     owner: {
+//       avatar: `avatar-angelina.jpg`,
+//       name: `Angelina`,
+//       isSuper: true,
+//     },
+//   }];
 
 const getOffers = (cityId) => {
   return offers.filter((offer) => offer.cityId === cityId);
@@ -164,15 +164,27 @@ it(`Reducer should change city`, () => {
 });
 
 it(`Reducer should get offer list for current city`, () => {
-  expect(reducer(state, {
+  const store = mockStore(reducer(state, {
     type: ActionType.GET_OFFER_LIST,
     payload: cities[0],
-  })).toEqual({
+  }));
+  store.dispatch({type: ActionType.GET_OFFER_LIST});
+  expect(store.getState()).toEqual({
     city: cities[1],
     offers: getOffers(cities[0].id),
     activeSortItem: SortType.DEFAULT,
     idActiveCard: `-1`
   });
+
+  // expect(reducer(state, {
+  //   type: ActionType.GET_OFFER_LIST,
+  //   payload: cities[0],
+  // })).toEqual({
+  //   city: cities[1],
+  //   offers: getOffers(cities[0].id),
+  //   activeSortItem: SortType.DEFAULT,
+  //   idActiveCard: `-1`
+  // });
 });
 
 it(`Reducer should change current card`, () => {
@@ -193,48 +205,7 @@ it(`Reducer should change sort offer list`, () => {
     payload: SortType.LOW_TO_HIGH,
   })).toEqual({
     city: cities[1],
-    offers: [{
-      id: `2`,
-      cityId: `2`,
-      photos: OFFER_IMAGES,
-      name: OFFER_NAMES[1],
-      rating: 3,
-      price: 120,
-      type: OFFER_TYPES[1],
-      isPremium: false,
-
-      descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-      countRooms: 2,
-      maxGuests: 2,
-      appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-      coordinates: [48.856663, 2.351556],
-      owner: {
-        avatar: `avatar-angelina.jpg`,
-        name: `Angelina`,
-        isSuper: true,
-      },
-    },
-    {
-      id: `3`,
-      cityId: `2`,
-      photos: OFFER_IMAGES,
-      name: OFFER_NAMES[1],
-      rating: 2,
-      price: 220,
-      type: OFFER_TYPES[1],
-      isPremium: false,
-
-      descriptions: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam`],
-      countRooms: 6,
-      maxGuests: 6,
-      appliances: [`Wi-Fi`, `Washing machine`, `Towels`, `Heating`],
-      coordinates: [48.856663, 2.351556],
-      owner: {
-        avatar: `avatar-angelina.jpg`,
-        name: `Angelina`,
-        isSuper: true,
-      },
-    }],
+    offers: getOffers(cities[1].id),
     activeSortItem: SortType.LOW_TO_HIGH,
     idActiveCard: `-1`
   });
